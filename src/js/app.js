dbfe28538da4137fbed72df2c0d954c611d06652
Vladimir Kalinin <Vofamchic@mail.ru>
2024-02-27 23:33:55 +0300

    modified:   src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -11,21 +11,21 @@ document.addEventListener('DOMContentLoaded', () => {
     "style",
     "grid-template-columns: repeat(4, 100px)"
   );
-  const cell = document.querySelectorAll(".cell");
+  const cell = document.querySelectorAll(".cell")
 
-  let getRandom = () => Math.floor(Math.random() * cell.length);
-  let lastTarget = getRandom();
+  let getRandom = () => Math.floor(Math.random() * cell.length)
+  let lastTarget = getRandom()
   const appendActiveByIndex = (index) =>
-    cell[index].classList.add("activeCell");
+    cell[index].classList.add("activeCell")
   const removeActiveByIndex = (index) =>
-    cell[index].classList.remove("activeCell");
+    cell[index].classList.remove("activeCell")
 
   const intervalHandler = () => {
-    removeActiveByIndex(lastTarget);
-    lastTarget = getRandom();
-    appendActiveByIndex(lastTarget);
-    timeout = setTimeout(intervalHandler, 1000);
-  };
+    removeActiveByIndex(lastTarget)
+    lastTarget = getRandom()
+    appendActiveByIndex(lastTarget)
+    timeout = setTimeout(intervalHandler, 1000)
+  }
 
-  timeout = setTimeout(intervalHandler, 1000);
+  timeout = setTimeout(intervalHandler, 1000)
 })
